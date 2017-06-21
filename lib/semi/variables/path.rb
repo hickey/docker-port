567f9b3bbc8d14e977997f555f1a83325671cb53
Gerard Hickey <Gerard.Hickey@smartsheet.com>
2017-06-21 13:56:41 -0700

    Implemented scoring to attempt to identify file paths

--- lib/semi/variables/path.rb
+++ lib/semi/variables/path.rb
@@ -4,6 +4,7 @@ module Semi::Variables
   class Path < Semi::Variables::Base
 
     @@path_re = Regexp.new('^(?<path>(?:\/|\.{1,2}\/|~(?:[a-z_][a-z0-9_]{0,30})?\/?|[^~][^\/\000]+\/)*?)(?<file>[^\/\000]+)?$')
+    @@path_score_threshold = 0.200
 
     def initialize(val)
       if @@path_re.match(val)
@@ -20,10 +21,25 @@ module Semi::Variables
     def self.validate(value)
       if ['String', 'Semi::Variables::Path'].include? value.class.to_s
         if @@path_re.match(value.to_s)
-          return true
+          if path_score(value.to_s) > @@path_score_threshold
+            return true
+          end
         end
       end
       false
+    end
+    
+    # provide a simple scoring method to assist in identifying
+    # a string as a path. 0.0 is a pure string with no path
+    # markers, 
+    def self.path_score(path)
+      return 0.0 if path.empty?
+
+      # use path "special" chars to calculate the score
+      metach = path.count('/.~')
+      plen = path.length
+      #puts "#{path} m/p/s = #{metach}/#{plen}/#{1.0 - (((plen - metach) * 1.0) / (plen + metach))}"
+      1.0 - (((plen - metach)*1.0) / (plen + metach))
     end
     
     def path
